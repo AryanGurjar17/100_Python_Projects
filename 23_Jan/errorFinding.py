adc8643bd9bd467f558c88de0aad29913b341c7b
Aryan Gurjar <your_email@example.com>
2026-01-23 22:13:56 +0530

    learned debug different ways

---
+++ 23_Jan/errorFinding.py
@@ -0,0 +1,19 @@
+from random import randint
+dice_images = ["0","0","0","0","0","0"]
+dice_num = randint(1,5)
+print(dice_images[dice_num])
+
+
+
+year = int(input("What your year of birth?"))
+
+print("you are a millennial.")
+
+print("you are a gen z.")
+
+ #wrong
+#bug find with with the print()
+
+
+
+
