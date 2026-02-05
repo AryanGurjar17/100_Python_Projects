6f3e8f4130931cb1174eaad9e89df05587929ff3
Aryan Gurjar <your_email@example.com>
2026-02-05 12:20:30 +0530

    higher_lowergame

---
+++ 4_feb/game_data.py
@@ -0,0 +1,60 @@
+data = [
+    {'name': 'Instagram',
+    'follower_count': 200,
+     'description': 'social media ',
+     'country': 'us'
+     },
+
+    {'name': 'Aryan',
+    'follower_count': 1000,
+     'description': 'sociaddl media ',
+     'country': 'india'
+     },
+    {'name': 'Aryan10',
+    'follower_count': 10,
+     'description': 'socwwwial media ',
+     'country': 'us'
+     },
+
+    {'name': 'Aryan09',
+    'follower_count': 9,
+     'description': 'social media ',
+     'country': 'us'
+     },
+    {'name': 'Aryan08',
+     'follower_count': 8,
+     'description': 'social media ',
+     'country': 'us'
+     },
+    {'name': 'Aryan07',
+     'follower_count': 7,
+     'description': 'social media ',
+     'country': 'us'
+     },
+    {'name': 'Aryan06',
+     'follower_count': 6,
+     'description': 'social media ',
+     'country': 'us'
+     },
+    {'name': 'Aryan05',
+     'follower_count': 5,
+     'description': 'social media ',
+     'country': 'us'
+     },
+    {'name': 'Aryan04',
+     'follower_count': 4,
+     'description': 'social media ',
+     'country': 'us'
+     },
+    {'name': 'Aryan03',
+     'follower_count': 3,
+     'description': 'social media ',
+     'country': 'us'
+     },
+    {'name': 'Aryan02',
+     'follower_count': 2,
+     'description': 'social media ',
+     'country': 'us'
+     },
+
+]
